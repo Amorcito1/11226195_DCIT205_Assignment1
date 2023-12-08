2a39b7bbe56dd8a8488b017bf15aeb50750a2fcc
Amorcito1 <149332725+Amorcito1@users.noreply.github.com>
2023-12-08 21:27:50 +0000

    Add files via upload

---
+++ DashboardPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DashboardPage = () => {
+  // Display student's current grades and missing grade alerts
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      {/* Display grades and alerts */}
+    </div>
+  );
+};
+
+export default DashboardPage;
